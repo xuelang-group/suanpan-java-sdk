05e316c7800b18c53613ad7a8dd6de74ecd8df81
xxchenqing <chenqing.lcq@xuelangyun.com>
2022-11-30 17:26:21 +0800

    add service invocation port

--- src/main/java/com/xuelang/mqstream/entity/Graph.java
+++ src/main/java/com/xuelang/mqstream/entity/Graph.java
@@ -1,12 +1,16 @@
 package com.xuelang.mqstream.entity;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
+import java.rmi.UnexpectedException;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Stream;
 
 @Slf4j
@@ -14,13 +18,6 @@ import java.util.stream.Stream;
 public class Graph {
     private JSONObject processes;
     private List<GraphConnection> connections;
-
-
-//    private Graph(Builder builder) {
-//        this.processes = builder.processes;
-//        this.connections = builder.connections;
-//
-//    }
 
     public JSONObject getProcesses() {
         return this.processes;
@@ -30,16 +27,54 @@ public class Graph {
         return this.connections;
     }
 
-    public String filter(String srcNodeId, String port) {
+    /**
+     * 根据port名获取portUUID
+     * */
+    public String getPortUUIDWithPortName(String nodeId,String port) throws UnexpectedException {
+        JSONArray ports=this.processes.getJSONObject(nodeId).getJSONObject("metadata").getJSONObject("def").getJSONArray("ports");
+        AtomicReference<String> result=new AtomicReference<>();
+        ports.forEach( item->{
+            String portUUID=JSON.parseObject(item.toString()).getString("uuid");
+            String portZH=JSON.parseObject(item.toString()).getJSONObject("description").getString("zh_CN");
+            String portEN=JSON.parseObject(item.toString()).getJSONObject("description").getString("en_US");
+            if (portUUID.equals(port) || (StringUtils.isNotBlank(portZH) && portZH.equals(port)) || (StringUtils.isNotBlank(portEN) && portEN.equals(port))) {
+                result.set(portUUID);
+                return;
+            }
+        });
+        if(result.get()==null||result.get().equals("")){
+            log.error("port-{}'s UUID not found!",port);
+            throw new UnexpectedException("port-" + port + "\'name not found!");
+        }
+        return result.get();
+    }
+
+    /**
+     * 根据portUUID及nodeId获取端口名称参数
+     */
+    public String getPortNameWithPortUUID(String nodeId, String portUUID) throws UnexpectedException {
+        JSONArray ports = this.processes.getJSONObject(nodeId).getJSONObject("metadata").getJSONObject("def").getJSONArray("ports");
+        AtomicReference<String> result = new AtomicReference<>();
+        ports.forEach(item -> {
+            String port = JSON.parseObject(item.toString()).getString("uuid");
+            String portZH = JSON.parseObject(item.toString()).getJSONObject("description").getString("zh_CN");
+            String portEN = JSON.parseObject(item.toString()).getJSONObject("description").getString("en_US");
+            if (portUUID.equals(port) || (StringUtils.isNotBlank(portZH)&&portZH.equals(port)) || (StringUtils.isNotBlank(portEN)&&portEN.equals(port))) {
+                result.set(portZH);
+                return;
+            }
+        });
+        if (result.get() == null || result.get().equals("")) {
+            log.error("port-{}'s name not found!", portUUID);
+            throw new UnexpectedException("port-" + portUUID + "\'name not found!");
+        }
+        return result.get();
+    }
+
+    public JSONObject filter(String srcNodeId, String port) throws UnexpectedException {
         if (connections.size() > 0) {
-            log.info("connections:" + JSON.toJSONString(this.connections).toString());
             Stream<GraphConnection> connectionStream = this.connections.stream().filter((GraphConnection c) -> {
 
-                log.info("c:" + JSON.toJSON(c).toString());
-                log.info("process:" + c.getSrc().getString("process"));
-                log.info("srcNodeId:" + srcNodeId);
-                log.info("port:" + c.getSrc().getString("port"));
-                log.info("port1" + port);
                 if (c.getSrc().getString("process").equals(srcNodeId) && c.getSrc().getString("port").equals(port)
                 ) {
                     return true;
@@ -48,32 +83,9 @@ public class Graph {
             });
             Optional<GraphConnection> conn = connectionStream.findFirst();
             if (conn == null) return null;
-            return conn.get().getTgt().getString("process");
+            return conn.get().getTgt();
         }
-        log.info("there is no connections in app graph");
-        return null;
+        throw new UnexpectedException("there is no connections in app graph");
     }
-
-//    public static class Builder {
-//        JSONObject processes;
-//        List<Connection> connections;
-//
-//        public Builder() {
-//        }
-//
-//        public Builder processes(JSONObject processes) {
-//            this.processes = processes;
-//            return this;
-//        }
-//
-//        public Builder connections(JSONArray connections) {
-//            this.connections = connections.toJavaList(Connection.class);
-//            return this;
-//        }
-//
-//        public Graph build() {
-//            return new Graph(this);
-//        }
-//    }
 }
 
--- src/main/java/com/xuelang/service/NodeSiblingService.java
+++ src/main/java/com/xuelang/service/NodeSiblingService.java
@@ -11,36 +11,55 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.io.IOException;
+import java.rmi.UnexpectedException;
 
 @Slf4j
 public class NodeSiblingService {
     @Autowired
     private OkHttpUtil httpClient;
     private AffinityRequest request = new AffinityRequest();
+    private Graph graph;
 
-    public String lookup(String port) throws IOException {
+    public String lookup(String port) throws Exception {
         log.info("start to lookup {} service name", port);
-        String url = request.getUrl("/appcontroller/graph/" + GlobalConfig.userId + "/" + GlobalConfig.appId);
-        String content = httpClient.get(url);
-        if (StringUtils.isNotEmpty(content)) {
-            JSONObject jsonObject = JSON.parseObject(content);
-            if (jsonObject.getObject("success", Boolean.class)) {
-                String graphJsonStr = jsonObject.getJSONObject("data").getJSONObject("graphJsonStr").toJSONString();
-                JSONObject graphJson = JSONObject.parseObject(graphJsonStr);
-                Graph graph = Graph.builder().processes(graphJson.getJSONObject("processes")).connections(graphJson.getJSONArray("connections").toJavaList(GraphConnection.class)).build();
-                String targetNodeId = graph.filter(GlobalConfig.nodeId, port);
-                if (targetNodeId == null || targetNodeId.equals("")) {
-                    log.info("there is no output node");
-                }
-                log.info("get service name for nodeId-{}", targetNodeId);
-                return getSiblingServiceName(targetNodeId);
+        try {
+            String url = request.getUrl("/appcontroller/graph/" + GlobalConfig.userId + "/" + GlobalConfig.appId);
+            String content = httpClient.get(url);
+            if (!StringUtils.isNotEmpty(content)) {
+                throw new UnexpectedException("get service graph error: null content!");
             }
+            JSONObject jsonObject = JSON.parseObject(content);
+            if (!jsonObject.getObject("success", Boolean.class)) {
+                throw new UnexpectedException("get service graph error!");
+            }
+            String graphJsonStr = jsonObject.getJSONObject("data").getJSONObject("graphJsonStr").toJSONString();
+            JSONObject graphJson = JSONObject.parseObject(graphJsonStr);
+            graph = Graph.builder().processes(graphJson.getJSONObject("processes")).connections(graphJson.getJSONArray("connections").toJavaList(GraphConnection.class)).build();
+
+            String portUUID = graph.getPortUUIDWithPortName(GlobalConfig.nodeId, port);
+            JSONObject tgt = graph.filter(GlobalConfig.nodeId, portUUID);
+            String targetNodeId = tgt.getString("process");
+            String targetNodeInPort = tgt.getString("port");
+
+            String inPortName = graph.getPortNameWithPortUUID(targetNodeId, targetNodeInPort);
+            String ip = this.getServiceIp(targetNodeId);
+
+            return String.format("%s:%s", ip, inPortName);
+        } catch (Exception e) {
+            log.error("get service invocation err:{}", e.getMessage());
+            throw new Exception("get service invocation err:{}", e);
         }
-        return null;
     }
 
     private String getSiblingServiceName(String targetNodeId) {
         return String.format("%s-%s-%s", "app", GlobalConfig.appId, targetNodeId);
+    }
+
+    private String getServiceIp(String targetNodeId) throws Exception {
+        if (targetNodeId == null || targetNodeId.equals("")) {
+            throw new Exception("there is no output node");
+        }
+        log.info("get service name for nodeId-{}", targetNodeId);
+        return getSiblingServiceName(targetNodeId);
     }
 }

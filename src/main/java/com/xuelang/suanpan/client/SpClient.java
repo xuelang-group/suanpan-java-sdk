a32db2d0c8771da6702bcb8cca4da649dd956e75
yunteng <yunteng.phl@xuelangyun.com>
2024-02-29 20:07:16 +0800

    initial SpClient class

---
+++ src/main/java/com/xuelang/suanpan/client/SpClient.java
@@ -0,0 +1,13 @@
+package com.xuelang.suanpan.client;
+
+/**
+ *  算盘平台客户端，既是算盘平台的sdk，通过构建SpClient的实例，即可进行和算盘平台的交互
+ *  SpClient中会包含Configuration,Event,Service,State,Stream领域服务，提供对应的领域接口
+ */
+public class SpClient {
+    public SpClient() {
+    }
+
+    public SpClient(SpConnectionParam spConnectionParam) {
+    }
+}

317203ef61c2014d5702638ce1de5b9992becf51
yunteng <yunteng.phl@xuelangyun.com>
2024-03-04 11:07:43 +0800

    状态领域服务实现类

---
+++ src/main/java/com/xuelang/suanpan/state/StateImpl.java
@@ -0,0 +1,8 @@
+package com.xuelang.suanpan.state;
+
+import com.xuelang.suanpan.common.BaseSpDomainEntity;
+
+public class StateImpl extends BaseSpDomainEntity implements IState{
+    private StateImpl() {
+    }
+}

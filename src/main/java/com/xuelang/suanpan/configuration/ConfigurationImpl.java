cab5ce5601058a28208e1f18267296465f5918b4
yunteng <yunteng.phl@xuelangyun.com>
2024-03-04 11:06:54 +0800

    配置领域服务实现类

---
+++ src/main/java/com/xuelang/suanpan/configuration/ConfigurationImpl.java
@@ -0,0 +1,8 @@
+package com.xuelang.suanpan.configuration;
+
+import com.xuelang.suanpan.common.BaseSpDomainEntity;
+
+public class ConfigurationImpl extends BaseSpDomainEntity implements IConfiguration{
+    private ConfigurationImpl() {
+    }
+}

891a36a611a0205a0fdcfd2953d22d0b701e9266
yunteng <yunteng.phl@xuelangyun.com>
2024-03-04 11:07:53 +0800

    服务调用领域服务实现类

---
+++ src/main/java/com/xuelang/suanpan/service/ServiceImpl.java
@@ -0,0 +1,8 @@
+package com.xuelang.suanpan.service;
+
+import com.xuelang.suanpan.common.BaseSpDomainEntity;
+
+public class ServiceImpl extends BaseSpDomainEntity implements IService{
+    private ServiceImpl() {
+    }
+}

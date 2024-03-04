e0a1172bf65fba8035887460d0fb4b7156731134
yunteng <yunteng.phl@xuelangyun.com>
2024-03-04 11:07:06 +0800

    事件领域服务实现类

---
+++ src/main/java/com/xuelang/suanpan/event/EventImpl.java
@@ -0,0 +1,8 @@
+package com.xuelang.suanpan.event;
+
+import com.xuelang.suanpan.common.BaseSpDomainEntity;
+
+public class EventImpl extends BaseSpDomainEntity implements IEvent{
+    private EventImpl() {
+    }
+}

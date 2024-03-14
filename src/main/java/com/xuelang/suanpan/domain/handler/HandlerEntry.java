c271e33d5eab1ed5b4d92a8114e22a493dda5727
yunteng <yunteng.phl@xuelangyun.com>
2024-03-14 17:38:55 +0800

    handler entry实体

---
+++ src/main/java/com/xuelang/suanpan/domain/handler/HandlerEntry.java
@@ -0,0 +1,44 @@
+package com.xuelang.suanpan.domain.handler;
+
+import com.xuelang.suanpan.domain.io.OutPort;
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class HandlerEntry<T> {
+    private T instance;
+    private Method method;
+    private boolean sync;
+    private List<OutPort> outPorts;
+
+    public T getInstance() {
+        return instance;
+    }
+
+    public void setInstance(T instance) {
+        this.instance = instance;
+    }
+
+    public Method getMethod() {
+        return method;
+    }
+
+    public void setMethod(Method method) {
+        this.method = method;
+    }
+
+    public boolean isSync() {
+        return sync;
+    }
+
+    public void setSync(boolean sync) {
+        this.sync = sync;
+    }
+
+    public List<OutPort> getOutPorts() {
+        return outPorts;
+    }
+
+    public void setOutPorts(List<OutPort> outPorts) {
+        this.outPorts = outPorts;
+    }
+}

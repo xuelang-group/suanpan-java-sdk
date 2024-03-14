ebe3d04dc15ce8db52808c071d5f1a679a58e5cd
yunteng <yunteng.phl@xuelangyun.com>
2024-03-14 17:21:07 +0800

    stream消息中的extra实体

---
+++ src/main/java/com/xuelang/suanpan/stream/entities/Extra.java
@@ -0,0 +1,15 @@
+package com.xuelang.suanpan.stream.entities;
+
+import com.alibaba.fastjson.JSONObject;
+
+class Extra {
+    private JSONObject global;
+
+    public JSONObject getGlobal() {
+        return global;
+    }
+
+    public void setGlobal(JSONObject global) {
+        this.global = global;
+    }
+}

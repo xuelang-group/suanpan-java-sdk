acbe4b80d75809bdb51a10e62e57eaa93c9c9b5d
yunteng <yunteng.phl@xuelangyun.com>
2024-03-14 17:13:44 +0800

    rename

--- src/main/java/com/xuelang/suanpan/annotation/AsyncHandlerMapping.java
+++ src/main/java/com/xuelang/suanpan/annotation/AsyncHandlerMapping.java
@@ -5,16 +5,16 @@ import java.lang.annotation.*;
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface AsyncSubscriber {
+public @interface AsyncHandlerMapping {
     /**
      * 异步订阅的输入桩端口序号
      * @return 输入桩端口序号
      */
-    int inport_index() default 0;
+    int inport_index() default -1;
 
     /**
      * 处理结果发送的输出桩端口集合
      * @return 输出桩端口集合
      */
-    int[] outport_index();
+    int[] default_outport_index();
 }

bb14392b642e1f2221eef6496b09b335984f746d
yunteng <yunteng.phl@xuelangyun.com>
2024-03-14 17:17:13 +0800

    同步处理器注解

--- src/main/java/com/xuelang/suanpan/annotation/SyncHandlerMapping.java
+++ src/main/java/com/xuelang/suanpan/annotation/SyncHandlerMapping.java
@@ -5,16 +5,16 @@ import java.lang.annotation.*;
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface SyncSubscriber {
+public @interface SyncHandlerMapping {
     /**
      * 同步订阅的输入桩端口序号集合
      * @return 输入桩端口序号集合
      */
-    int[] inport_index();
+    int[] inport_index() default -1;
 
     /**
      * 处理结果发送的输出桩端口集合
      * @return 输出桩端口集合
      */
-    int[] outport_index();
+    int[] default_outport_index();
 }

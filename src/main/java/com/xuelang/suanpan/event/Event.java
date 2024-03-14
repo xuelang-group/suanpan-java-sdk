7a59836cb2e6c4f7ff25640465f5ff674efaacf5
yunteng <yunteng.phl@xuelangyun.com>
2024-03-14 17:19:40 +0800

    rename

--- src/main/java/com/xuelang/suanpan/event/Event.java
+++ src/main/java/com/xuelang/suanpan/event/Event.java
@@ -2,7 +2,7 @@ package com.xuelang.suanpan.event;
 
 import com.xuelang.suanpan.common.BaseSpDomainEntity;
 
-public class EventImpl extends BaseSpDomainEntity implements IEvent{
-    private EventImpl() {
+public class Event extends BaseSpDomainEntity {
+    private Event() {
     }
 }

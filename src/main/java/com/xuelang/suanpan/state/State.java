1fd72469af3ac3448087b6422af65f7c2be81d23
yunteng <yunteng.phl@xuelangyun.com>
2024-03-14 17:18:11 +0800

    rename

--- src/main/java/com/xuelang/suanpan/state/State.java
+++ src/main/java/com/xuelang/suanpan/state/State.java
@@ -2,7 +2,7 @@ package com.xuelang.suanpan.state;
 
 import com.xuelang.suanpan.common.BaseSpDomainEntity;
 
-public class StateImpl extends BaseSpDomainEntity implements IState{
-    private StateImpl() {
+public class State extends BaseSpDomainEntity {
+    private State() {
     }
 }

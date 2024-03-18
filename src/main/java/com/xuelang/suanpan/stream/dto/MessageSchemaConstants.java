722d48dba1653673c0a051e3cea30dded2d0f456
yunteng <yunteng.phl@xuelangyun.com>
2024-03-18 16:23:07 +0800

    新增消息schema key

--- src/main/java/com/xuelang/suanpan/stream/dto/MessageSchemaConstants.java
+++ src/main/java/com/xuelang/suanpan/stream/dto/MessageSchemaConstants.java
@@ -2,7 +2,8 @@ package com.xuelang.suanpan.stream.dto;
 
 public class MessageSchemaConstants {
     public static final String RECV_TYPE_KEY = "ReceiveMsgType";
-    public static final String REQUEST_ID_KEY = "id";
+    public static final String REQUEST_ID_KEY = "request_id";
+    public static final String REQUEST_ID_ALIAS_KEY = "id";
     public static final String EXTRA_KEY = "extra";
     public static final String SUCCESS_KEY = "success";
 }

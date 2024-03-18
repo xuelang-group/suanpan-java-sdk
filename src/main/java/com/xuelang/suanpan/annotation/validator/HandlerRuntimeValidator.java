6ada03c9212d997b9ad2cfb685f450e258292798
yunteng <yunteng.phl@xuelangyun.com>
2024-03-18 14:50:42 +0800

    handler运行时校验输入输出端口合法性

--- src/main/java/com/xuelang/suanpan/annotation/validator/HandlerRuntimeValidator.java
+++ src/main/java/com/xuelang/suanpan/annotation/validator/HandlerRuntimeValidator.java
@@ -1,12 +1,10 @@
 package com.xuelang.suanpan.annotation.validator;
 
 import com.xuelang.suanpan.annotation.AsyncHandlerMapping;
-import com.xuelang.suanpan.annotation.SuanpanHandlerResponseBody;
 import com.xuelang.suanpan.annotation.SyncHandlerMapping;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,34 +13,35 @@ public class HandlerRuntimeValidator {
     public static void validateHandlerPortValues(Class<?> clazz, int inPortMaxIndex, int outPortMaxIndex) throws RuntimeException {
         Method[] methods = clazz.getDeclaredMethods();
         List<Method> filteredMethods = Arrays.stream(methods).filter(method -> (method.isAnnotationPresent(AsyncHandlerMapping.class)
-                        || method.isAnnotationPresent(SyncHandlerMapping.class))).collect(Collectors.toList());
+                || method.isAnnotationPresent(SyncHandlerMapping.class))).collect(Collectors.toList());
 
-        for (Method method : filteredMethods) {
+        filteredMethods.stream().parallel().forEach(method -> {
+
             if (method.isAnnotationPresent(AsyncHandlerMapping.class)) {
                 AsyncHandlerMapping asyncHandlerMapping = method.getAnnotation(AsyncHandlerMapping.class);
                 if (asyncHandlerMapping.inport_index() > inPortMaxIndex) {
-                    throw new RuntimeException("illegal scope @AsyncSubscriber inport value set, class: "
-                            + clazz.getName() + " method: " + method.getName());
+                    throw new RuntimeException(AsyncHandlerMapping.class.getSimpleName()+" illegal scope inport value set, component max inport index is " + inPortMaxIndex + ", Clazz: "
+                            + clazz.getName() + ", Method: " + method.getName());
                 }
 
                 if (Arrays.stream(asyncHandlerMapping.default_outport_index()).anyMatch(index -> (index > outPortMaxIndex))) {
-                    throw new RuntimeException("illegal scope @AsyncSubscriber outport value set, class: "
-                            + clazz.getName() + " method: " + method.getName());
+                    throw new RuntimeException(AsyncHandlerMapping.class.getSimpleName()+" illegal scope outport value set, component max outport index is " + outPortMaxIndex + ", Clazz: "
+                            + clazz.getName() + ", Method: " + method.getName());
                 }
             }
 
             if (method.isAnnotationPresent(SyncHandlerMapping.class)) {
                 SyncHandlerMapping syncHandlerMapping = method.getAnnotation(SyncHandlerMapping.class);
                 if (Arrays.stream(syncHandlerMapping.inport_index()).anyMatch(index -> (index > inPortMaxIndex))) {
-                    throw new RuntimeException("illegal scope @SyncSubscriber inport value set, class: "
-                            + clazz.getName() + " method: " + method.getName());
+                    throw new RuntimeException(SyncHandlerMapping.class.getSimpleName()+" illegal scope inport value set, component max inport index is " + inPortMaxIndex + ", Clazz: "
+                            + clazz.getName() + ", Method: " + method.getName());
                 }
 
                 if (Arrays.stream(syncHandlerMapping.default_outport_index()).anyMatch(index -> (index > outPortMaxIndex))) {
-                    throw new RuntimeException("illegal scope @SyncSubscriber outport value set, class: "
-                            + clazz.getName() + " method: " + method.getName());
+                    throw new RuntimeException(SyncHandlerMapping.class.getSimpleName()+" illegal scope outport value set, component max outport index is " + outPortMaxIndex + ", Clazz: "
+                            + clazz.getName() + ", Method: " + method.getName());
                 }
             }
-        }
+        });
     }
 }

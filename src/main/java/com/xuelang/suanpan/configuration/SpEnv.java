b119a287433a4c303c0060d6b674d9f7343604b6
yunteng <yunteng.phl@xuelangyun.com>
2024-03-18 15:05:56 +0800

    bugfix： 消息队列中存储的string，boolean不能转义

--- src/main/java/com/xuelang/suanpan/configuration/SpEnv.java
+++ src/main/java/com/xuelang/suanpan/configuration/SpEnv.java
@@ -11,6 +11,8 @@ public class SpEnv {
     private static Map<String, InPort> inPortMap = new HashMap<>();
     private static Map<String, OutPort> outPortMap = new HashMap<>();
     private static Map<String, Object> spParamMap = new HashMap<>();
+    private static Integer Max_InPort_Index = null;
+    private static Integer Max_OutPort_Index = null;
 
     static {
         String spNodeInfoBase64Str = System.getenv("SP_NODE_INFO");
@@ -18,28 +20,46 @@ public class SpEnv {
             JSONObject spNodeInfo = JSONObject.parseObject(new String(Base64.getDecoder().decode(spNodeInfoBase64Str)));
             JSONObject inputs = spNodeInfo.getJSONObject("inputs");
             if (inputs != null && !inputs.isEmpty()) {
+                final int[] maxInportIndex = {-1};
                 inputs.values().stream().forEach(v -> {
                     InPort inPort = new InPort();
-                    String uuid = ((JSONObject) v).getString("uuid");
+                    inPort.setUuid(((JSONObject) v).getString("uuid"));
                     inPort.setName(((JSONObject) v).getString("name"));
                     inPort.setType(((JSONObject) v).getString("type"));
                     inPort.setSubType(((JSONObject) v).getString("subtype"));
                     inPort.setDescription(((JSONObject) v).getString("description"));
-                    inPortMap.put(uuid, inPort);
+                    inPortMap.put(inPort.getUuid(), inPort);
+                    int index = Integer.valueOf(inPort.getUuid().substring(2));
+                    if (index> maxInportIndex[0]){
+                        maxInportIndex[0] = index;
+                    }
                 });
+
+                if (maxInportIndex[0]>0){
+                    Max_InPort_Index = maxInportIndex[0];
+                }
             }
 
             JSONObject outputs = spNodeInfo.getJSONObject("outputs");
             if (outputs != null && !outputs.isEmpty()) {
+                final int[] maxOutportIndex = {-1};
                 outputs.values().stream().forEach(v -> {
                     OutPort outPort = new OutPort();
-                    String uuid = ((JSONObject) v).getString("uuid");
+                    outPort.setUuid(((JSONObject) v).getString("uuid"));
                     outPort.setName(((JSONObject) v).getString("name"));
                     outPort.setType(((JSONObject) v).getString("type"));
                     outPort.setSubType(((JSONObject) v).getString("subtype"));
                     outPort.setDescription(((JSONObject) v).getString("description"));
-                    outPortMap.put(uuid, outPort);
+                    outPortMap.put(outPort.getUuid(), outPort);
+                    int index = Integer.valueOf(outPort.getUuid().substring(3));
+                    if (index> maxOutportIndex[0]){
+                        maxOutportIndex[0] = index;
+                    }
                 });
+
+                if (maxOutportIndex[0]>0){
+                    Max_OutPort_Index = maxOutportIndex[0];
+                }
             }
         }
     }
@@ -69,18 +89,23 @@ public class SpEnv {
         return outPortMap.get(key);
     }
 
+    public static InPort getInPortByIndex(Integer inportIndex) {
+        String key = "in" + inportIndex;
+        return inPortMap.get(key);
+    }
+
     public static List<OutPort> getOutPorts() {
         return new ArrayList<>(outPortMap.values());
     }
 
     public static Long getQueueMaxSendLen() {
         Object value = get(ConfigurationKeys.streamSendQueueMaxLengthKey);
-        return value != null ? (Long) value : null;
+        return value != null ? Long.valueOf(value.toString()) : 1000L;
     }
 
-    public static Boolean getQueueSendTrim() {
+    public static boolean getQueueSendTrim() {
         Object value = get(ConfigurationKeys.streamSendQueueTrimImmediatelyKey);
-        return value != null ? (Boolean) value : null;
+        return value != null ? Boolean.valueOf(value.toString()) : false;
     }
 
     public static String getNodeId() {
@@ -93,9 +118,14 @@ public class SpEnv {
         return value != null ? (String) value : null;
     }
 
-    public static Boolean getEnableP2pSend() {
+    public static String getReceiveQueue(){
+        Object value = get(ConfigurationKeys.streamRecvQueueKey);
+        return value != null ? (String) value : null;
+    }
+
+    public static boolean getEnableP2pSend() {
         Object value = get(ConfigurationKeys.enableP2pSendKey);
-        return value != null ? (Boolean) value : null;
+        return value != null ? Boolean.valueOf(value.toString()) : false;
     }
 
     public static String getStreamHost() {
@@ -105,7 +135,7 @@ public class SpEnv {
 
     public static Integer getStreamPort() {
         Object value = get(ConfigurationKeys.mqRedisPortKey);
-        return value != null ? (Integer) value : null;
+        return value != null ? Integer.valueOf(value.toString()) : null;
     }
 
     private static Object get(String key) {
@@ -122,11 +152,13 @@ public class SpEnv {
         return spParamMap.get(key);
     }
 
-    public static int getMaxInPortIndex() {
-        return 0;
+    public static Integer getMaxInPortIndex() {
+        return Max_InPort_Index;
     }
 
-    public static int getMaxOutPortIndex() {
-        return 0;
+    public static Integer getMaxOutPortIndex() {
+        return Max_OutPort_Index;
     }
+
+
 }

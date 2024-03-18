abb67854a6e1ff763cf798469aa9e4aa083d9b7a
yunteng <yunteng.phl@xuelangyun.com>
2024-03-18 16:27:00 +0800

    spClient接口定义

--- src/main/java/com/xuelang/suanpan/client/ISpClient.java
+++ src/main/java/com/xuelang/suanpan/client/ISpClient.java
@@ -1,19 +1,19 @@
 package com.xuelang.suanpan.client;
 
-import com.xuelang.suanpan.configuration.IConfiguration;
-import com.xuelang.suanpan.event.IEvent;
-import com.xuelang.suanpan.service.IService;
-import com.xuelang.suanpan.state.IState;
+import com.xuelang.suanpan.configuration.Configuration;
+import com.xuelang.suanpan.event.Event;
+import com.xuelang.suanpan.service.Service;
+import com.xuelang.suanpan.state.State;
 import com.xuelang.suanpan.stream.IStream;
 
 public interface ISpClient {
     IStream stream();
 
-    IConfiguration configuration();
+    Configuration configuration();
 
-    IEvent event();
+    Event event();
 
-    IService service();
+    Service service();
 
-    IState state();
+    State state();
 }

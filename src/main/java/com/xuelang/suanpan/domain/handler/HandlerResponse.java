d34e12834f382758ab6e6b2b27aba6aec7a8656d
yunteng <yunteng.phl@xuelangyun.com>
2024-03-18 14:49:46 +0800

    链式构建实现

--- src/main/java/com/xuelang/suanpan/domain/handler/HandlerResponse.java
+++ src/main/java/com/xuelang/suanpan/domain/handler/HandlerResponse.java
@@ -2,37 +2,49 @@ package com.xuelang.suanpan.domain.handler;
 
 import com.xuelang.suanpan.configuration.SpEnv;
 import com.xuelang.suanpan.domain.io.OutPort;
-import org.apache.commons.collections4.CollectionUtils;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class HandlerResponse {
     private Map<OutPort, Object> outPortDataMap = new HashMap<>();
+    private List<Object> nonSpecifiedOutPortDataList = new ArrayList<>();
 
-    public void appendData(@Nullable Integer outPortIndex, Object data) throws RuntimeException {
-        if (outPortIndex == null) {
-            List<OutPort> outPorts = SpEnv.getOutPorts();
-            if (CollectionUtils.isEmpty(outPorts)){
-                throw new RuntimeException("current component has no outPort, can not append return data");
-            }
-            outPorts.stream().forEach(outPort -> {
-                outPortDataMap.put(outPort, data);
-            });
-        } else {
-            OutPort outPort;
-            if ((outPort = SpEnv.getOutPortByIndex(outPortIndex)) == null) {
-                throw new RuntimeException("current component has no this outPort: " + "out" + outPortIndex);
-            }
-
-            // TODO: 2024/3/12 根据输出端口数据类型，将data转成对应的类型
-            outPortDataMap.put(outPort, data);
-        }
+    public static ResponseBuilder builder(){
+        return new ResponseBuilder();
     }
-
     public Map<OutPort, Object> getOutPortDataMap() {
         return outPortDataMap;
+    }
+
+    public List<Object> getNonSpecifiedOutPortDataList() {
+        return nonSpecifiedOutPortDataList;
+    }
+
+    public static class ResponseBuilder{
+        private HandlerResponse response = new HandlerResponse();
+        public ResponseBuilder append(@Nullable Integer outPortIndex, Object data) throws RuntimeException {
+            if (outPortIndex == null) {
+                response.getNonSpecifiedOutPortDataList().add(data);
+            } else {
+                OutPort outPort;
+                if ((outPort = SpEnv.getOutPortByIndex(outPortIndex)) == null) {
+                    throw new RuntimeException("current component has no this outPort: " + "out" + outPortIndex);
+                }
+
+                // TODO: 2024/3/12 根据输出端口数据类型，将data转成对应的类型
+                response.getOutPortDataMap().put(outPort, data);
+            }
+
+            return this;
+        }
+
+
+        public HandlerResponse build(){
+            return this.response;
+        }
     }
 }

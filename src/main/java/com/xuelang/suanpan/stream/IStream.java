eae2a9d4e72a9fdf3846799a92b71f54068746fc
yunteng <yunteng.phl@xuelangyun.com>
2024-03-20 20:55:45 +0800

    暴露给用户的stream接口

--- src/main/java/com/xuelang/suanpan/stream/IStream.java
+++ src/main/java/com/xuelang/suanpan/stream/IStream.java
@@ -1,5 +1,24 @@
 package com.xuelang.suanpan.stream;
 
+import com.xuelang.suanpan.stream.handler.HandlerRequest;
+import com.xuelang.suanpan.stream.message.StreamContext;
+
+import java.util.concurrent.TimeUnit;
+
 public interface IStream {
-    void pub();
+    /**
+     * 发送消息
+     * @param outPorts
+     * @param data
+     * @return
+     */
+    String publish(StreamContext streamContext);
+
+    /**
+     * 主动轮询输入端口的消息
+     * @param timeout 超时时间
+     * @param unit 超时时间单位
+     * @return
+     */
+    HandlerRequest polling(long timeout, TimeUnit unit);
 }

352f04aba3cf8156ad8595b76a2fccaa30df6b29
yunteng <yunteng.phl@xuelangyun.com>
2024-03-20 18:51:58 +0800

    节点接收数据类型枚举

---
+++ src/main/java/com/xuelang/suanpan/node/io/NodeReceiveMsgType.java
@@ -0,0 +1,22 @@
+package com.xuelang.suanpan.node.io;
+
+import org.apache.commons.lang3.StringUtils;
+
+public enum NodeReceiveMsgType {
+    sync,
+    async;
+
+    public static NodeReceiveMsgType getByCode(String code) {
+        if (StringUtils.isBlank(code)) {
+            return null;
+        }
+        for (NodeReceiveMsgType type : NodeReceiveMsgType.values()) {
+            if (type.name().equals(code)) {
+                return type;
+            }
+        }
+
+        return null;
+
+    }
+}

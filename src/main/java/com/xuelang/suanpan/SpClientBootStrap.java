a916d20bf0681ae7b84b89861a615ac62ceafb84
yunteng <yunteng.phl@xuelangyun.com>
2024-03-21 16:28:49 +0800

    bootstrap实现

---
+++ src/main/java/com/xuelang/suanpan/SpClientBootStrap.java
@@ -0,0 +1,21 @@
+package com.xuelang.suanpan;
+
+import com.xuelang.suanpan.client.ISpClient;
+import com.xuelang.suanpan.client.SpClientBuilder;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 整体sdk的bootstrap类，用于初始化sdk
+ */
+@Slf4j
+public class SpClientBootStrap {
+
+    static {
+        // todo 执行初始化逻辑
+        ISpClient spClient = SpClientBuilder.build();
+        spClient.stream();
+
+    }
+
+
+}

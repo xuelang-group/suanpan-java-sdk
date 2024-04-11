74540aa9c2408cfd4e568ea7d74e9ef6a2582798
yunteng <yunteng.phl@xuelangyun.com>
2024-04-11 10:58:46 +0800

    修改接口

--- src/main/java/com/xuelang/suanpan/stream/client/AbstractMqClient.java
+++ src/main/java/com/xuelang/suanpan/stream/client/AbstractMqClient.java
@@ -5,11 +5,9 @@ import com.xuelang.suanpan.stream.handler.HandlerProxy;
 import com.xuelang.suanpan.stream.message.InflowMessage;
 import com.xuelang.suanpan.stream.message.MetaOutflowMessage;
 
-import java.util.concurrent.TimeUnit;
-
 public abstract class AbstractMqClient implements Observer {
     protected HandlerProxy proxy;
-    public abstract InflowMessage polling(long timeout, TimeUnit unit);
+    public abstract InflowMessage polling(long timeoutMillis);
     public abstract void infiniteConsume();
     public abstract String publish(MetaOutflowMessage metaOutflowMessage);
     public abstract void destroy();

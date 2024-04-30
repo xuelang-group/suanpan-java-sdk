79fe02d05fdf79513a3d799340a71535135f13bc
yunteng <yunteng.phl@xuelangyun.com>
2024-04-30 14:10:51 +0800

    新增异常类型

--- src/main/java/com/xuelang/suanpan/common/exception/GlobalExceptionType.java
+++ src/main/java/com/xuelang/suanpan/common/exception/GlobalExceptionType.java
@@ -8,8 +8,8 @@ public enum GlobalExceptionType {
     NoSuchOutPortException(104, "no such outPort exception"),
     NoSuchMethodException(105, "no such method exception"),
     DuplicationHandlerException(106,"duplication handler exception"),
-
-    IllegalStreamOperation(107, "subscriber existed, cannot use polling together!");
+    IllegalStreamOperation(107, "subscriber existed, cannot use polling together!"),
+    IllegalParameter(109, "illegal parameter");
 
     private int code;
     private String message;
